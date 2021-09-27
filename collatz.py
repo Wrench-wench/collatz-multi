d6d83ea6723d0d75b2ccaa468fee29d7e7962ace
Wrench-wench <WrenchWench@protonmail.com>
2021-09-27 23:50:36 +0100

    Stared a working function

--- collatz.py
+++ collatz.py
@@ -3,25 +3,35 @@ import multiprocessing
 print('Number of CPUs:', multiprocessing.cpu_count())
 
 
-def collatz(numberloops):
+def collatz(starting_number) -> dict:
+    assert starting_number > 1
 
-    steplist = []
-    number = 0
+    step_dict = {}
+    number = starting_number
     steps = 0
+    steps_history = []
 
     while number != 1:
 
-                if (number % 2) == 0:                  # If the number is even, divide it by two.
-                    number = number // 2               # Integer division where the remainder is discarded: //
-                    steplist.append(number)
-                    steps += 1
+        if (number % 2) == 0:
+            number = number // 2
+        else:
+            number = (number * 3) + 1
+        steps += 1
+        steps_history.append(number)
 
-                else:                                  # If the number is odd, triple it and add one.
-                    number = (number * 3) + 1
-                    steplist.append(number)
-                    steps += 1
+    step_dict[starting_number] = steps_history
+    print(step_dict)
+    return step_dict
 
 
-    print(steplist)
+# How to add a key and value in one line:
+# step_dict['UniquekeyName'] = 'Value'
 
 collatz(10)
+collatz(3)
+run = collatz(30)
+
+print('Result for the number', next(iter(run)))
+steps = (next(iter(run.items())))
+print('Number of steps', len(steps[1]))
