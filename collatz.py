03a246d33f44e622540afe83a70e097424a2ab32
Wrench-wench <WrenchWench@protonmail.com>
2021-09-28 00:25:12 +0100

    Got a basic multiprocessing example working

--- collatz.py
+++ collatz.py
@@ -4,15 +4,20 @@ print('Number of CPUs:', multiprocessing.cpu_count())
 
 
 def collatz(starting_number) -> dict:
-    assert starting_number > 1
 
     step_dict = {}
     number = starting_number
     steps = 0
     steps_history = []
 
-    while number != 1:
+    try:
+        assert starting_number > 1
+    except AssertionError as a:
+        steps_history.append(starting_number)
+        step_dict[starting_number] = steps_history
+        return step_dict
 
+    while number != 1:
         if (number % 2) == 0:
             number = number // 2
         else:
@@ -27,12 +32,10 @@ def collatz(starting_number) -> dict:
 # How to add a key and value in one line:
 # step_dict['UniquekeyName'] = 'Value'
 
-runs = 30
+if __name__ == "__main__":
+    results = []
+    for run in range(2,31):
+        run = multiprocessing.Process(target=collatz, args=(run,))
+        results.append(run)
+        run.start()
 
-while runs > 1:
-    run = collatz(runs)
-    steps = (next(iter(run.items())))
-
-    # Print results
-    print('Result for the number', next(iter(run)), 'Number of steps', len(steps[1]))
-    runs += -1
