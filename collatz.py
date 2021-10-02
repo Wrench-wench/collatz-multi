59550528f20c93df7c3ee1387f251748848b8f8f
Wrench-wench <WrenchWench@protonmail.com>
2021-10-02 12:05:59 +0100

    Reverted this to 3bb4c8e

--- collatz.py
+++ collatz.py
@@ -1,16 +1,23 @@
-import timeit
+import multiprocessing
+import time
 
-import_module = 'import multiprocessing'
+print('Number of CPUs:', multiprocessing.cpu_count())
 
-code = """
-#print('Number of CPUs:', multiprocessing.cpu_count())
 
 def collatz(starting_number) -> dict:
 
     step_dict = {}
+    number = starting_number
     steps = 0
     steps_history = []
-    number = starting_number
+
+    try:
+        assert starting_number > 1
+    except AssertionError as a:
+        steps_history.append(starting_number)
+        step_dict[starting_number] = steps_history
+        Q.put(step_dict)
+        return
 
     while number != 1:
         if (number % 2) == 0:
@@ -21,17 +28,24 @@ def collatz(starting_number) -> dict:
         steps_history.append(number)
 
     step_dict[starting_number] = steps_history
+    #print('The number', starting_number, 'has', len(steps_history), 'steps.')
     Q.put(step_dict)
 
 
-runs = 20
-Q = multiprocessing.Queue()
-for run in range(2,(runs + 1)): # Minimum for range must be 2
-    run = multiprocessing.Process(target=collatz, args=(run,))
-    run.start()
-    Q.get()
-    run.join()"""
+# How to add a key and value in one line:
+# step_dict['UniquekeyName'] = 'Value'
 
-starttime = timeit.default_timer()
-print('Time taken for the first 20 numbers, 1 thousand times:', timeit.timeit(stmt=code, setup=import_module, number=1000))
-print('Time taken:', round(((timeit.default_timer() - starttime) / 60),2), 'minutes')
+if __name__ == "__main__":
+    time_start = time.time()
+
+    runs = 1000000
+
+    Q = multiprocessing.Queue()
+    for run in range((runs + 1)):
+        run = multiprocessing.Process(target=collatz, args=(run,))
+        run.start()
+        Q.get()
+        run.join()
+
+    print(f'Time taken for {runs} runs:', time.time() - time_start)
+
