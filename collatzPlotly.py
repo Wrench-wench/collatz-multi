84a5fb72bf4684eaf2e6e1c5c7a1e4ef7b2fe80a
Wrench-wench <WrenchWench@protonmail.com>
2021-10-02 19:47:09 +0100

    Created version that plots results to a graph

---
+++ collatzPlotly.py
@@ -0,0 +1,64 @@
+import plotly.express as px
+import multiprocessing
+import pandas as pd
+import time
+
+print('Number of CPUs:', multiprocessing.cpu_count())
+
+def collatz(starting_number) -> dict:
+
+    step_dict = {}
+    number = starting_number
+    steps = 0
+    steps_history = []
+
+    try:
+        assert starting_number > 1
+    except AssertionError as a:
+        steps_history.append(starting_number)
+        step_dict[starting_number] = steps_history
+        Q.put(step_dict)
+        return
+
+    while number != 1:
+        if (number % 2) == 0:
+            number = number // 2
+        else:
+            number = (number * 3) + 1
+        steps += 1
+        steps_history.append(number)
+
+    step_dict[starting_number] = steps_history
+    #print('The number', starting_number, 'has', len(steps_history), 'steps.')
+    Q.put(step_dict)
+
+if __name__ == "__main__":
+    time_start = time.time()
+    dict_list = []
+    runs = 10000
+
+    Q = multiprocessing.Queue()
+    for run in range(2,(runs + 1)):
+        dict_name = f'run-{run}'
+        run = multiprocessing.Process(target=collatz, args=(run,))
+        run.start()
+        dict_name = Q.get()
+        dict_list.append(dict_name)
+        run.join()
+
+    full_dict = {}
+    for dict in dict_list:
+        full_dict = {**full_dict, **dict} # Shallow merge two dicts: z = {**x, **y}
+
+    for k,v in full_dict.items(): #
+        newvalue = {k : len(v)}
+        full_dict.update(newvalue)
+
+    df = pd.DataFrame.from_dict(full_dict,orient ='index')
+    print(df)
+
+    for k,v in full_dict.items():
+        fig = px.scatter(df,)
+    fig.show()
+
+    print(f'Time taken for {runs} runs:', time.time() - time_start)
